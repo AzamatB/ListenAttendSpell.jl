8e861fb8ec93ce85269ad58ed9eaa1981a0049b8
AzamatB <aberdysh@gmail.com>
2019-08-27 20:36:16 +0600

    Add files via upload

---
+++ LAS.jl
@@ -0,0 +1,271 @@
+# Listen, Attend and Spell: arxiv.org/abs/1508.01211
+using Flux
+using Flux: reset!, onehotbatch, throttle, train!, @treelike, @epochs
+using IterTools
+using JLD2
+using StatsBase
+# using CuArrays
+
+
+struct BLSTM{L,D}
+   forward  :: L
+   backward :: L
+   output   :: D
+end
+
+@treelike BLSTM
+
+function BLSTM(in::Integer, hidden::Integer, out::Integer, σ = identity)
+   forward  = LSTM(in, hidden)
+   backward = LSTM(in, hidden)
+   output   = Dense(2hidden, out, σ)
+   return BLSTM(forward, backward, output)
+end
+
+BLSTM(D_in::Integer, D_out::Integer) = BLSTM(D_in, ceil(Int, (D_in + D_out)/2), D_out)
+
+(m::BLSTM)(X::AbstractMatrix)::AbstractMatrix = m.output([m.forward(X); reverse(m.backward(reverse(X; dims=2)); dims=2)])
+
+Flux.reset!(m::BLSTM) = Flux.reset!((m.forward, m.backward))
+
+restack(X::AbstractMatrix)::AbstractMatrix = [X[:,1:2:size(X,2)]; X[:,2:2:size(X,2)]]
+
+"""
+   PBLSTM(D_in::Integer, D_out::Integer)
+
+Pyramidal BLSTM is the same as BLSTM, with the addition that the outputs of BLSTM are concatenated at consecutive steps.
+"""
+function PBLSTM(D_in::Integer, D_out::Integer)
+   iseven(D_out) || throw("output dimension of the pyramidal BLSTM layer must be even")
+   D_out_blstm = Int(D_out/2)
+   return Chain(BLSTM(D_in, D_out_blstm), restack)
+end
+
+"""
+   Encoder(layer_sizes)
+   Encoder(D_in::Integer, D_out::Integer; nlayers::Integer = 4)
+   Encoder(D_in::Integer, D_out::Integer, hidden_sizes)
+
+Encoder that consists of block of PBLSTMs. It accepts filter bank spectra as inputs and acts as acoustic model encoder.
+"""
+function Encoder(layer_sizes)
+   (length(layer_sizes) ≥ 3) || throw("number of layers of Encoder must be ≥ 2")
+   layer_dims = Tuple(partition(layer_sizes, 2, 1))
+   layers = ( PBLSTM(D_in, D_out) for (D_in, D_out) ∈ layer_dims[1:end-1] )
+   model = Chain(layers..., BLSTM(layer_dims[end]...))
+   return model
+end
+
+function Encoder(D_in::Integer, D_out::Integer; nlayers::Integer = 4)
+   layer_sizes = range(D_in, D_out; length=nlayers+1)
+   layer_sizes = map(layer_sizes) do x
+      n = ceil(Int, x)
+      n = iseven(n) ? n : (n + 1)
+      return n
+   end
+   layer_sizes[1]   = D_in
+   layer_sizes[end] = D_out
+   return Encoder(layer_sizes)
+end
+
+Encoder(D_in::Integer, D_out::Integer, hidden_sizes) = Encoder((D_in, hidden_sizes..., D_out))
+
+
+function MLP(layer_sizes, σs)
+   layers = Tuple(Dense(D_in, D_out, σ) for ((D_in, D_out), σ) ∈ zip(partition(layer_sizes, 2, 1), σs))
+   model = length(layers) == 1 ? first(layers) : Chain(layers...)
+   return model
+end
+
+function MLP(layer_sizes, σ::Function)
+   σs = ntuple(i -> σ, length(layer_sizes))
+   return MLP(layer_sizes, σs)
+end
+
+function MLP(D_in::Integer, D_out::Integer, σs)
+   layer_sizes = ceil.(Int, range(D_in, D_out; length=length(σs)+1))
+   return MLP(layer_sizes, σs)
+end
+
+function MLP(D_in::Integer, D_out::Integer, σ::Function=identity; nlayers::Integer = 1)
+   σs = ntuple(i -> σ, nlayers)
+   return MLP(D_in, D_out, σs)
+end
+
+
+function Decoder(layer_sizes)
+   layers = ( LSTM(D_in, D_out) for (D_in, D_out) ∈ partition(layer_sizes, 2, 1) )
+   model = Chain(layers...)
+   return model
+end
+
+function Decoder(D_in::Integer, D_out::Integer; nlayers::Integer = 2)
+   layer_sizes = ceil.(Int, range(D_in, D_out; length=nlayers+1))
+   return Decoder(layer_sizes)
+end
+
+Decoder(D_in::Integer, D_out::Integer, hidden_sizes) = Decoder((D_in, hidden_sizes..., D_out))
+
+
+function CharacterDistribution(D_in::Integer, D_out::Integer, σ::Function; nlayers::Integer, log::Bool=true)
+   f = log ? logsoftmax : softmax
+   layer_sizes = ceil.(Int, range(D_in, D_out; length=nlayers+1))
+   layer_dims = Tuple(partition(layer_sizes, 2, 1))
+   layers = ( Dense(D_in, D_out, σ) for (D_in, D_out) ∈ layer_dims[1:end-1] )
+   return Chain(layers..., Dense(layer_dims[end]...), f)
+end
+
+CharacterDistribution(D_in::Integer, D_out::Integer; log::Bool=true) = Chain(Dense(D_in, D_out), log ? logsoftmax : softmax)
+
+
+mutable struct State{T <: AbstractVector}
+   context    :: T   # last attention context
+   decoding   :: T   # last decoder state
+   prediction :: T   # last prediction
+end
+
+@treelike State
+
+function Flux.reset!(s::State)
+   s.context    = param(zero(s.context))
+   s.decoding   = param(zero(s.decoding))
+   s.prediction = param(zero(s.prediction))
+end
+
+
+struct LAS{E, Dϕ, Dψ, L, C, S}
+   listen      :: E   # encoder function
+   attention_ϕ :: Dϕ  # attention context function
+   attention_ψ :: Dψ  # attention context function
+   spell       :: L   # RNN decoder
+   infer       :: C   # character distribution inference function
+   state       :: S   # current state of the model
+end
+
+@treelike LAS
+
+function LAS(D_in::Integer, D_out::Integer;
+             D_encoding::Integer,
+             D_attention::Integer,
+             D_decoding::Integer)
+   listen      = Encoder(D_in, D_encoding)
+   attention_ϕ = MLP(D_decoding, D_attention)
+   attention_ψ = MLP(D_encoding, D_attention)
+   spell       = Decoder(D_encoding + D_decoding + D_out, D_decoding)
+   infer       = CharacterDistribution(D_encoding + D_decoding, D_out)
+   context₀    = param(zeros(Float32, D_encoding))
+   decoding₀   = param(zeros(Float32, D_decoding))
+   prediction₀ = param(zeros(Float32, D_out))
+   las = LAS(listen, attention_ϕ, attention_ψ, spell, infer, State(context₀, decoding₀, prediction₀)) |> gpu
+   return las
+end
+
+function (m::LAS)(X::AbstractMatrix)::AbstractMatrix
+   # compute input encoding
+   H = m.listen(gpu(pad(X)))
+   # precompute ψ(H)
+   ψH = m.attention_ψ(H)
+   # initialize prediction
+   Ŷ = m.state.prediction[:,1:0]
+   for _ ∈ axes(X,2)
+      # compute decoder state
+      m.state.decoding = m.spell([m.state.decoding; m.state.prediction; m.state.context])
+      # compute attention context
+      αs = softmax(ψH'm.attention_ϕ(m.state.decoding))
+      m.state.context = sum( α * h for (α, h) ∈ zip(αs, eachcol(H)) )
+      # predict probability distribution over character alphabet
+      m.state.prediction = m.infer([m.state.decoding; m.state.context])
+      # concatenate with previous character predictions
+      Ŷ = [Ŷ m.state.prediction]
+   end
+   # reset!(m::LAS)
+   return Ŷ
+end
+
+function Flux.reset!(m::LAS)
+   reset!(m.listen)
+   reset!(m.spell)
+   reset!(m.state)
+   return nothing
+end
+
+function pad(X::AbstractMatrix; multiplicity::Integer=8)::AbstractMatrix
+   T = size(X,2)
+   Δ = ceil(Int, T / multiplicity)multiplicity - T
+   x_min = minimum(X)
+   X = [X fill(x_min, size(X,1), Δ)]
+   return X
+end
+
+
+# load data
+X, y, Xs_train, ys_train, Xs_eval, ys_eval, Xs_val, ys_val, Xs_test, ys_test, PHN2IDX, PHN_IDCS = let val_set_size = 32
+   JLD2.@load "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_MFCC/data_test.jld" Xs ys PHN2IDX PHN_IDCS
+   Xs_val, ys_val, Xs_test, ys_test = Xs[1:val_set_size], ys[1:val_set_size], Xs[(val_set_size+1):end], ys[(val_set_size+1):end]
+   JLD2.@load "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_MFCC/data_train.jld" Xs ys PHN2IDX PHN_IDCS
+   eval_idcs = sample(eachindex(ys), val_set_size; replace=false)
+   Xs_eval, ys_eval = Xs[eval_idcs], ys[eval_idcs]
+   first(Xs), first(ys), Xs, ys, Xs_eval, ys_eval, Xs_val, ys_val, Xs_test, ys_test, PHN2IDX, PHN_IDCS
+end
+
+
+const D_x = size(X,1)
+const D_y = length(PHN_IDCS)
+
+D_encoding = 128
+D_attention = 64 # attention dimension
+D_decoding = 256
+
+D_feed_forward = 128
+D_LSTM_speller = 512
+
+
+const las = LAS(D_x, D_y; D_encoding=D_encoding, D_attention=D_attention, D_decoding=D_decoding)
+
+function loss(X::AbstractMatrix, y::AbstractVector)::Real
+   Ŷ = las(X)
+   # compute l = -sum( ŷ[i] for (ŷ, i) ∈ zip(eachcol(Ŷ), y) )
+   # in a GPU friendly way, i.e. without scalar indexing
+   num_phn = size(Ŷ,1)
+   idcs = map((n, i) -> num_phn*n + i, 0:(length(y)-1), y)
+   l = -sum(Ŷ[idcs])
+   return l
+end
+
+loss(Xs::AbstractVector{<:AbstractMatrix}, ys::AbstractVector{<:AbstractVector})::Real = sum(loss.(Xs, ys))
+
+show_loss_val() = @show(loss(Xs_val, ys_val))
+show_loss_eval() = @show(loss(Xs_eval, ys_eval))
+
+@time las(X)
+@time @show loss(Xs_val, ys_val)
+@time @show loss(Xs_eval, ys_eval)
+
+θ = params(las)
+optimizer = ADAM()
+data = zip(Xs_train, ys_train)
+
+@time @epochs 10 begin
+   @time train!(loss, θ, data, optimizer;
+      cb = throttle(show_loss_eval, 60))
+   loss_val = loss(X_val, Y_val)
+   @show loss_val
+   if loss_val < loss_val_saved
+      loss_val_saved = loss_val
+      @save "/Users/Azamat/Projects/LAS/models/TIMIT/LAS.jld2" model optimiser
+   end
+end
+
+
+
+
+
+# beamsearch()
+
+using Levenstein
+
+function cer()
+end
+
+function wer()
+end
---
+++ data_TIMIT.jl
@@ -0,0 +1,131 @@
+using WAV
+using MFCC
+using Flux: onehotbatch
+using JLD2
+using Base.Iterators
+using IterTools
+
+# make dictionary to map from phones to class numbers
+const PHN2IDX, PHN_IDCS = let
+   phonemes = split("h#	q	eh	dx	iy	r	ey	ix	tcl	sh	ow	z	s	hh	aw	m	t	er	l	w	aa	hv	ae	dcl	y	axr	d	kcl	k	ux	ng	gcl	g	ao	epi	ih	p	ay	v	n	f	jh	ax	en	oy	dh	pcl	ah	bcl	el	zh	uw	pau	b	uh	th	ax-h	em	ch	nx	eng")
+   phn_idcs = eachindex(phonemes)
+   phn2idx = Dict(phoneme => i for (i, phoneme) ∈ enumerate(phonemes))
+   phn2idx["sil"] = phn2idx["h#"]
+   phn2idx, phn_idcs
+end
+
+# make dictionary to perform class folding
+const FOLDINGS = Dict(
+   "ao" => "aa",
+   "ax" => "ah",
+   "ax-h" => "ah",
+   "axr" => "er",
+   "hv" => "hh",
+   "ix" => "ih",
+   "el" => "l",
+   "em" => "m",
+   "en" => "n",
+   "nx" => "n",
+   "eng" => "ng",
+   "zh" => "sh",
+   "pcl" => "sil",
+   "tcl" => "sil",
+   "kcl" => "sil",
+   "bcl" => "sil",
+   "dcl" => "sil",
+   "gcl" => "sil",
+   "h#" => "sil",
+   "pau" => "sil",
+   "epi" => "sil",
+   "ux" => "uw"
+)
+
+const FRAME_LENGTH = 0.025 # ms
+const FRAME_INTERVAL = 0.010 # ms
+
+"""
+   build_features(wavfile::AbstractString, phnfile::AbstractString)
+
+Extracts Mel filterbanks and associated labels from `wavfile` and `phnfile`.
+"""
+function build_features(wavfile::AbstractString, phnfile::AbstractString, Δorder::Integer=2)
+   samples, sampling_frequency = wavread(wavfile)
+   samples = vec(samples)
+   mfccs, _, _ = mfcc(samples, sampling_frequency, :rasta; wintime=FRAME_LENGTH, steptime=FRAME_INTERVAL)
+
+   lines = readlines(phnfile)
+   labels = similar(lines)
+   boundaries = similar(lines, Int)
+   for (i, line) ∈ enumerate(lines)
+      _, boundary, labels[i] = split(line)
+      boundaries[i] = parse(Int, boundary)
+   end
+
+   sampl_length = FRAME_LENGTH * sampling_frequency
+   sampl_interval = FRAME_INTERVAL * sampling_frequency
+   half_frame_length = FRAME_LENGTH / 2
+
+   # begin generating sequence labels by looping through the MFCC frames
+   nsegments = length(labels)
+   label_idx = 1
+   label_sequence = similar(labels, size(mfccs,1))
+   label, boundary = labels[label_idx], boundaries[label_idx]
+   for i ∈ axes(mfccs, 1)
+      win_end = sampl_length + (i-1)sampl_interval
+      # move on to next label if current frame of samples is more than half
+      # way into next labeled section and there are still more labels to iterate through
+      if (label_idx < nsegments) && (win_end - boundary > half_frame_length)
+         label_idx += 1
+         label, boundary = labels[label_idx], boundaries[label_idx]
+      end
+      label_sequence[i] = label
+   end
+   # get rid of the frames that were labeld as "q"
+   idcs_to_keep = filter(i -> label_sequence[i] != "q", eachindex(label_sequence))
+   label_sequence = label_sequence[idcs_to_keep]
+   mfccs = mfccs[idcs_to_keep,:]
+   # compute filterbank derivates, fitted over 2 consecutive frames
+   Δmfccs = (Δmfcc for Δmfcc ∈ take(iterated(x -> deltas(x, 2), mfccs), Δorder))
+   features = mapreduce(x -> x', vcat, (mfccs, Δmfccs...))
+   # convert from double precision to single precision for Flux
+   features = convert(Matrix{Float32}, features)
+   # generate class numbers, there are 61 total classes, but only 39 are used after folding
+   target = [PHN2IDX[label] for label ∈ label_sequence]
+   return (features, target)
+end
+
+"""
+   generate_data(dir_data::AbstractString, path_out::AbstractString)
+
+Extracts data from files in `dir_data`, builds features, concatenates produced outputs and saves results as `path_out`.
+"""
+function generate_data(dir_data::AbstractString, path_out::AbstractString; Δorder::Integer=2)
+   Xys = mapreduce(vcat, walkdir(dir_data)) do (root, _, files)
+      print("$(root)\r")
+      phnfiles = (file for file ∈ files if !startswith(file, "SA") && endswith(file, ".PHN"))
+      wavfiles = (file for file ∈ files if !startswith(file, "SA") && endswith(file, ".wav"))
+
+      map(wavfiles, phnfiles) do (wavfile, phnfile)
+         wavfilepath = joinpath(root, wavfile)
+         phnfilepath = joinpath(root, phnfile)
+         build_features(wavfilepath, phnfilepath, Δorder)
+      end
+   end
+   # Xs, ys = fieldarrays(StructArray(Xys))
+   Xs = [X for (X, _) ∈ Xys]
+   ys = [y for (_, y) ∈ Xys]
+
+   ispath(path_out) || mkpath(dirname(path_out))
+   JLD2.@save path_out Xs ys PHN2IDX PHN_IDCS
+   return Xs, ys
+end
+
+
+const DIR_DATA_TEST  = "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_wav/TEST"
+const PATH_OUT_TEST   = "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_MFCC/data_test.jld"
+
+const DIR_DATA_TRAIN = "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_wav/TRAIN"
+const PATH_OUT_TRAIN  = "/Users/Azamat/Projects/LAS/data/TIMIT/TIMIT_MFCC/data_train.jld"
+
+@time generate_data(DIR_DATA_TEST, PATH_OUT_TEST)
+@time generate_data(DIR_DATA_TRAIN, PATH_OUT_TRAIN)
